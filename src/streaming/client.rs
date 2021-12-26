b64693a077d61b3ce53d6814c4e04ab6b397c232
jimmykiang <castlevanity@gmail.com>
2021-12-26 09:02:14 +0100

    enable server side TLS

--- src/streaming/client.rs
+++ src/streaming/client.rs
@@ -2,11 +2,25 @@ pub mod pb {
     tonic::include_proto!("grpc.examples.echo");
 }
 
+use tonic::transport::{Certificate, Channel, ClientTlsConfig};
 use pb::{echo_client::EchoClient, EchoRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = EchoClient::connect("http://[::1]:50051").await.unwrap();
+    let pem = tokio::fs::read("../grpc/tls/ca.crt").await?;
+    let ca = Certificate::from_pem(pem);
+    let tls = ClientTlsConfig::new()
+        .ca_certificate(ca)
+        .domain_name("localhost");
+
+    // let mut client = EchoClient::connect("http://[::1]:50051").await.unwrap();
+
+    let channel = Channel::from_static("http://[::1]:50051")
+        .tls_config(tls)?
+        .connect()
+        .await?;
+
+    let mut client = EchoClient::new(channel);
 
     let request= EchoRequest {
         message: "foo".into()
--- src/streaming/server.rs
+++ src/streaming/server.rs
@@ -6,6 +6,7 @@ use std::net::ToSocketAddrs;
 use tonic::{transport::Server, Request, Response, Status};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
+use tonic::transport::{Identity, ServerTlsConfig};
 
 use pb::{EchoRequest, EchoResponse};
 
@@ -43,8 +44,14 @@ impl pb::echo_server::Echo for EchoServer {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cert = tokio::fs::read("../grpc/tls/server.crt").await?;
+    let key = tokio::fs::read("../grpc/tls/server.pem").await?;
+
+    let identity = Identity::from_pem(cert, key);
+
     let server = EchoServer {};
     Server::builder()
+        .tls_config(ServerTlsConfig::new().identity(identity))?
         .add_service(pb::echo_server::EchoServer::new(server))
         .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
         .await
